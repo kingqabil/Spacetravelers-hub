63ddf1200457d2ca0775e7602304c24f4430763f
Afiz Savage <savageafiz9@gmail.com>
2022-01-31 15:12:58 +0000

    Add profile view and navigation link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Rockets from './components/rockets/Rockets';
 import Missions from './components/missions/Missions';
 import Dragons from './components/dragons/Dragons';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route exact path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/dragons" element={<Dragons />} />
-
+        <Route path="/myProfile" element={<MyProfile />} />
       </Routes>
     </>
   );
