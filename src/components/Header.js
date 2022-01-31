0c28f70fac4940d1069582ba3ea659285021b816
fikerte <tofikertesfaye@gmail.com>
2022-01-31 17:52:02 +0300

    Add Nav links and create folder structure for redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import Logo from '../images/001-planet.png';
 
 const Header = () => (
   <header>
     {' '}
-    <img src={Logo} alt="logo" />
-    <nav />
+    <div>
+      <img src={Logo} alt="logo" />
+      <h1>Space Traveler`&apos;`s Hub</h1>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="/missions">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dragons">Dragons</NavLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 );
 
