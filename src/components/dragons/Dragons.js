34e6ec592b580be295f58a0a4c4c47052658998d
fikerte <tofikertesfaye@gmail.com>
2022-02-01 17:50:58 +0300

    Render list of dragons in Dragons route

--- src/components/dragons/Dragons.js
+++ src/components/dragons/Dragons.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
 import { getDragonsFromAPI } from '../../redux/dragons/dragons';
 
 const Dragons = () => {
@@ -15,7 +16,22 @@ const Dragons = () => {
   }, []);
 
   return (
-    <h1>Dragons</h1>
+    <div className="dragons-list">
+      {
+dragonsData.map((dragon) => (
+  <div key={dragon.id} className="dragon">
+    <div className="dragon-img">
+      <img src={dragon.dragon_img} alt={dragon.dragon_name} />
+    </div>
+    <div className="dragon-description">
+      <h3>{dragon.dragon_name}</h3>
+      <p>{dragon.dragon_description}</p>
+      <Button variant="primary">Reserve Dragon</Button>
+    </div>
+  </div>
+))
+}
+    </div>
   );
 };
 
