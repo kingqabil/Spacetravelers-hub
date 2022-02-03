e7a42143693b3046af7403f284cc755bfc42b3dd
Afiz Savage <savageafiz9@gmail.com>
2022-02-03 12:46:41 +0000

    Save reserved mission to provife view

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReservedDragons from './dragons/ReservedDragons';
+import ReservedMissions from './missions/Reserved';
 
 const MyProfile = () => (
   <div className="profile-wrapper">
+    <ReservedMissions />
     <ReservedDragons />
   </div>
 );
