6695fa79354ccccf053b1bf2ffc2de92746f20e4
Afiz Savage <savageafiz9@gmail.com>
2022-02-04 17:13:58 +0000

    Test action-creator helper for mission component

--- src/__tests__/Missions.test.js
+++ src/__tests__/Missions.test.js
@@ -3,8 +3,9 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import Missions from '../components/missions/Missions';
+import { createAction } from '../redux/missions/missions';
 
-describe('Missions Component test', () => {
+describe('Missions Component snapshot test', () => {
   it('Renders Mission Page', () => {
     const app = renderer.create(
       <Provider store={store}>
@@ -13,5 +14,8 @@ describe('Missions Component test', () => {
       </Provider>,
     ).toJSON();
     expect(app).toMatchSnapshot();
+  });
+  it('Action creator work correctly', () => {
+    expect(createAction('add', '1234')).toStrictEqual({ type: 'add', payload: '1234' });
   });
 });
